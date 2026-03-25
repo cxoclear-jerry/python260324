5e1c3d6c5753026a77bead308d8746cc05224ef0
Jerry <cxoclear@gmail.com>
2026-03-25 16:47:10 +0900

    웹크롤링 연습2

--- web1.py
+++ web1.py
@@ -8,4 +8,12 @@ print(soup.prettify())
 # <p>를 검색
 # print(soup.find_all("p"))
 # print(soup.find("p"))
-print(soup.find_all("p", class_="outer-text"))
+# print(soup.find_all("p", class_="outer-text"))
+# 조건 검색 attrs를 사용
+# print(soup.find_all("p", attrs={"class":"outer-text"}))
+# id 로 검색
+# print(soup.find_all(id="first"))
+# 태그 내부의 문자열: .text
+for tag in soup.find_all("p"):
+    title = tag.text.strip()
+    print(title)
